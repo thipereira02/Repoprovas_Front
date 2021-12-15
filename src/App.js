032edc4923b374f4872d06ad4d9ecfc95deeef22
thipereira02 <thiago.spereira02@gmail.com>
2021-12-15 10:20:06 -0300

    Using eslint

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import GlobalStyle from "./layouts/GlobalStyles";
-import Home from "./pages/Home";
+import GlobalStyle from './layouts/GlobalStyles';
+import Home from './pages/Home';
 
 export default function App() {
     return (
         <>
-            <GlobalStyle /><BrowserRouter>
+            <GlobalStyle />
+            <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<Home />} />
                 </Routes>
