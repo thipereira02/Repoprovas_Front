8b7b5598b4154b8e2eeca60bd12f0a95d89d6047
thipereira02 <thiago.spereira02@gmail.com>
2021-12-19 21:13:21 -0300

    Changing BASE_URL

--- src/services/server.js
+++ src/services/server.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:4000';
+const BASE_URL = 'https://back-repoprovasapp.herokuapp.com';
 
 export function getCategories(){
     return axios.get(`${BASE_URL}/categories`);
